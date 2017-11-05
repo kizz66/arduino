bdb7ca0c786dbd517550ac14bdf15851692a70fd
Igor Kiz <kizz66@mail.ru>
2017-11-05 22:03:15 +0700

    wip

--- x-plane/x-plane.ino
+++ x-plane/x-plane.ino
@@ -4,6 +4,8 @@
 
 LiquidCrystal_I2C lcd(0x3f, 16, 2); // i2c address 0x27
 
+int outValues[8]; // 0-6 autopilot status 7- VS
+
 void setup()  {
   BoardNumber 1;// -- Assign Board Number here  (0...9)
   lcd.init();
@@ -20,32 +22,82 @@ void loop()   {
 
 void ProgOut(byte id, float val) {
 
-  switch (id) {
-
-    case 1: lcd.setCursor(0, 0);  if (val > 0)  lcd.print("FD");   else lcd.print("  ");
-      lcd.setCursor(14, 0); if (val == 2) lcd.print("AP");   else lcd.print("  ");   break;
-    case 2: lcd.setCursor(0, 1);  if (val == 2) lcd.print("HDG");  else lcd.print("   ");  break;
-    case 3: lcd.setCursor(0, 1);  if (val > 0)  lcd.print("BC");   else lcd.print("  ");   break;
-    case 4: lcd.setCursor(4, 1);  if (val > 0)  lcd.print("APPR"); else lcd.print("    "); break;
-    case 5: lcd.setCursor(4, 0);  if (val > 0)  lcd.print("NAV");  else lcd.print("   ");  break;
-    case 6: lcd.setCursor(9, 0);  if (val == 2) lcd.print("ALT");  else lcd.print("   ");  break;
-    case 7: lcd.setCursor(9, 0);  if (val == 2) lcd.print("V/S");  else lcd.print("   ");  break;
-    case 8: lcd.setCursor(10, 1); if (val == 1) lcd.print("YAW");  else lcd.print("   ");  break;
+  // AP status
+  if (id >= 1 && id <= 7 ) {
+    outValues[id - 1] = val;
+    renderApStatus();
+  }
+   // ALT / VS
+  if (id == 8 ) {
+    outValues[id - 1] = val;
+    renderAltAndVs();
   }
 }
 
+void renderAltAndVs(void) {
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print(outValues[7]);
+}
+
+void renderApStatus(void) {
+  lcd.clear();
+  lcd.setCursor(0, 0);  if (outValues[0] > 0)  lcd.print("FD");
+  lcd.setCursor(14, 0); if (outValues[0] == 2) lcd.print("AP");
+
+  lcd.setCursor(0, 1);  if (outValues[1] == 2) lcd.print("HDG");
+  lcd.setCursor(0, 1);  if (outValues[2] > 0)  lcd.print("BC ");
+   
+  lcd.setCursor(4, 1);  if (outValues[3] > 0)  lcd.print("APPR");
+  lcd.setCursor(4, 0);  if (outValues[4] > 0)  lcd.print("NAV");
+  
+  lcd.setCursor(9, 0);  if (outValues[5] == 2) lcd.print("ALT");
+  lcd.setCursor(9, 0);  if (outValues[6] == 2) lcd.print("V/S");
+}
+
 /*  Lines in the "data.cfg"  file:
-  570
-  1D 0 sim/cockpit2/autopilot/flight_director_mode
-  2D 0 sim/cockpit2/autopilot/heading_status
-  3D 0 sim/cockpit2/autopilot/backcourse_status
-  4D 0 sim/cockpit2/autopilot/approach_status
-  5D 0 sim/cockpit2/autopilot/nav_status
-  6D 0 sim/cockpit2/autopilot/altitude_hold_status
-  7D 0 sim/cockpit2/autopilot/vvi_status
-  8D 0 sim/cockpit2/switches/yaw_damper_on
-  9D 0 sim/cockpit2/switches/navigation_lights_on
+@ ================= INFO =================
+
+Board #1 - Arduino (USB) + i2c -------------------------------------------------------
+
+---- Digital pins: ----
 
 
-  10S 2 sim/cockpit2/controls/flap_handle_deploy_ratio 1 0,0.4,1 0,200,500
+---- Analog pins: ----
+
+2 - Vertical speed, at pre-selected VSI.
+3 - Altitude-hold ARM (pre-selected ALT)
+
+---- Programmable inputs: ----
+
+
+---- Programmable outputs: ----
+
+1 - Flight director mode
+2 - Heading mode status
+3 - Backcourse mode status
+4 - Approach mode status
+5 - Nav mode status
+6 - Altitude hold status
+7 - Vvi mode status
+8 - Combined Alt/VVI selector display shows VVI or ALT
+
+
+
+@ ================= CONFIG =================
+
+*1-4Ui
+[A]
+[I]
+A2+ sim/autopilot/vertical_speed_pre_sel
+A3+ sim/autopilot/altitude_arm
+[O]
+1D 0 sim/cockpit2/autopilot/flight_director_mode
+2D 0 sim/cockpit2/autopilot/heading_status
+3D 0 sim/cockpit2/autopilot/backcourse_status
+4D 0 sim/cockpit2/autopilot/approach_status
+5D 0 sim/cockpit2/autopilot/nav_status
+6D 0 sim/cockpit2/autopilot/altitude_hold_status
+7D 0 sim/cockpit2/autopilot/vvi_status
+10S 2 sim/cockpit2/controls/flap_handle_deploy_ratio 1 0,1 1000,1800
+8D 0 sim/cockpit2/autopilot/alt_vvi_is_showing_vvi 1
 */
