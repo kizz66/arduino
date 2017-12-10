5033966bc05aecbc0482b1153a96a795c97a81cb
Igor Kiz <kizz66@mail.ru>
2017-12-10 13:49:52 +0700

    wip

--- CellsBedLighting/CellsBedLighting.ino
+++ CellsBedLighting/CellsBedLighting.ino
@@ -1,53 +1,40 @@
 #include "IRremote.h"
-#include <Wire.h>
 
-IRrecv irrecv(2); // указываем вывод, к которому подключен приемник
+IRrecv irrecv(11); // указываем вывод, к которому подключен приемник
 
 decode_results results;
 //Заточена под пульт телевизора LG
-String const
-RED_KEY     = "20df4eb1",
-GREEN_KEY   = "20df8e71",
-YELLOW_KEY  = "20dfc639",
-BLUE_KEY    = "20df8679",
-REPEAT_KEY  = "ffffffff";
-
 int const
-ANALOG_OUT_PIN = 10;
+ANALOG_OUT_PIN = 3;
 
-String key = "";
-
-int
-value = 0,
-ratio = 10;
+int out = 0;
 
 void setup() {
-  //Serial.begin(9600); // выставляем скорость COM порта
   irrecv.enableIRIn(); // запускаем прием
   pinMode(ANALOG_OUT_PIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
 
   if ( irrecv.decode( &results )) { // если данные пришли
-    String result = String(results.value, HEX);
-
-    if (result != REPEAT_KEY) {
-      key = result;
-    }
-    if (key == RED_KEY) {
-
-    } else if (key == GREEN_KEY) {
-
-    } else  if (key == YELLOW_KEY) {
-      value += ratio;
-      value = value <= 255 ? value : 255;
-    } else if (key == BLUE_KEY) {
-      value -= ratio;
-      value = value >= 0 ? value : 0;
+    switch (results.value) {
+      case 0xd5257fdb:
+        digitalWrite(13, LOW);
+        analogWrite(ANALOG_OUT_PIN, 0);
+        break;
+      case 0x80a24e43:
+        digitalWrite(13, LOW);
+        analogWrite(ANALOG_OUT_PIN, 150);
+        break;
+      case 0xf3c645b:
+        digitalWrite(13, LOW);
+        analogWrite(ANALOG_OUT_PIN, 200);
+        break;
+      case 0x1fd3b9bf:
+        analogWrite(ANALOG_OUT_PIN, 255);
+        digitalWrite(13, HIGH);
     }
     irrecv.resume(); // принимаем следующую команду
-    analogWrite(ANALOG_OUT_PIN, value);
-    // Serial.println(value);
   }
 }
