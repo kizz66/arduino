6d265137e0c6e0bb367d4be16ccd202c65805c15
Igor Kiz <kizz66@mail.ru>
2017-12-30 18:20:20 +0700

    night lighting by ir control

--- CellsBedLighting/CellsBedLighting.ino
+++ CellsBedLighting/CellsBedLighting.ino
@@ -1,40 +1,50 @@
 #include "IRremote.h"
 
 IRrecv irrecv(11); // указываем вывод, к которому подключен приемник
-
 decode_results results;
+
 //Заточена под пульт телевизора LG
 int const
-ANALOG_OUT_PIN = 3;
-
-int out = 0;
+OUT_PIN = 3;
+int trigger = false;
 
 void setup() {
   irrecv.enableIRIn(); // запускаем прием
-  pinMode(ANALOG_OUT_PIN, OUTPUT);
+  pinMode(OUT_PIN, OUTPUT);
   pinMode(13, OUTPUT);
+
+  digitalWrite(OUT_PIN, LOW);// управляем длоком реле (низкий уровень) 
 }
-
 void loop() {
-
   if ( irrecv.decode( &results )) { // если данные пришли
     switch (results.value) {
-      case 0xd5257fdb:
-        digitalWrite(13, LOW);
-        analogWrite(ANALOG_OUT_PIN, 0);
+      case 0xd5257fdb: // RED
+
         break;
-      case 0x80a24e43:
-        digitalWrite(13, LOW);
-        analogWrite(ANALOG_OUT_PIN, 150);
+      case 0x80a24e43: // GREEN
+
         break;
-      case 0xf3c645b:
-        digitalWrite(13, LOW);
-        analogWrite(ANALOG_OUT_PIN, 200);
+      case 0xf3c645b: // YELLOW
+        turnOff();
         break;
-      case 0x1fd3b9bf:
-        analogWrite(ANALOG_OUT_PIN, 255);
-        digitalWrite(13, HIGH);
+      case 0x1fd3b9bf: // BLUE
+        turnOn();
     }
     irrecv.resume(); // принимаем следующую команду
   }
+  delay(50);
+}
+
+void turnOn() {
+  if (trigger) return;
+  trigger = true;
+  digitalWrite(OUT_PIN, LOW);
+  digitalWrite(13, HIGH);
+}
+
+void turnOff() {
+  if (!trigger) return;
+  trigger = false;
+  digitalWrite(OUT_PIN, HIGH);
+  digitalWrite(13, LOW);
 }
