a55d9a6784a175d51319a1acbbb0eaee38546230
Igor Kiz <kizz66@mail.ru>
2018-01-21 14:12:00 +0700

    wip

--- joker/joker.ino
+++ joker/joker.ino
@@ -1,9 +1,10 @@
 const int UP = 0,
           DOWN = 1;
 
-int led = 1, // 3 - UNO
+int led = 1,// 3 - UNO
+    motor = 0,
     val,
-    valueTriggedLevel = 550,
+    valueTriggedLevel = 650,
     outValue = 0,
     lightDuration = 5,
     direct = UP;
@@ -11,9 +12,10 @@ int led = 1, // 3 - UNO
 bool isFlash = false,
      flashTrigger = false;
 
-
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(motor, OUTPUT);
+  digitalWrite(motor, LOW);
 }
 
 void loop() {
@@ -35,17 +37,21 @@ void doFlash(void) {
   if (isFlash) {
     analogWrite(led, outValue);
     if (direct == UP) {
-      outValue = outValue+5;
+      outValue = outValue + 5;
       if (outValue > 255) {
         outValue = 255;
         direct = DOWN;
       }
     } else {
       outValue = outValue - 1;
+      if (outValue < 20) {
+        digitalWrite(motor, HIGH);
+      }
       if (outValue < 0 ) {
         outValue = 0;
         direct = UP;
         isFlash = false;
+        digitalWrite(motor, LOW);
       }
     }
   }
