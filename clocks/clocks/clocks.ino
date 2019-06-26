1cc6d561b2b3829b89278d5c3b95614978eae592
Igor Kiz <kizz66@mail.ru>
2019-06-26 23:22:11 +0700

    wip

--- clocks/clocks/clocks.ino
+++ clocks/clocks/clocks.ino
@@ -11,6 +11,41 @@
 #define D3 11 // ок
 #define D4 13 // ok
 
+// десятки часов, часы, десятки минут, минуты
+int displayData[4] = {1, 2, 3, 4};
+int displayDigit[4] = {DEC_HOURS, HOURS, DEC_MIN, MIN};
+
+int chars[10][4] = {
+  {LOW, LOW, LOW, LOW},
+  {LOW, LOW, LOW, HIGH},
+  {LOW, LOW, HIGH, LOW},
+  {LOW, LOW, HIGH, HIGH},
+  {LOW, HIGH, LOW, LOW},
+  {LOW, HIGH, LOW, HIGH},
+  {LOW, HIGH, HIGH, LOW},
+  {LOW, HIGH, HIGH, HIGH},
+  {HIGH, LOW, LOW, LOW},
+  {HIGH, LOW, LOW, HIGH},
+};
+
+void setValueToDigit(int value, int digitNumber) {
+  digitsOff();
+  delay(2);
+  digitalWrite(D1, chars[value][3]);
+  digitalWrite(D2, chars[value][2]);
+  digitalWrite(D3, chars[value][1]);
+  digitalWrite(D4, chars[value][0]);
+  digitalWrite(digitNumber, HIGH);
+  delay(3);
+}
+
+void digitsOff(void) {
+  digitalWrite(DEC_HOURS, LOW);
+  digitalWrite(HOURS, LOW);
+  digitalWrite(DEC_MIN, LOW);
+  digitalWrite(MIN, LOW);
+}
+
 void setup() {
   pinMode(DEC_HOURS, OUTPUT);
   pinMode(HOURS, OUTPUT);
@@ -21,68 +56,11 @@ void setup() {
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
-
-
-  digitalWrite(DEC_HOURS, HIGH);
-  digitalWrite(HOURS, HIGH);
-  digitalWrite(DEC_MIN, HIGH);
-  digitalWrite(MIN, HIGH);
-
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-  digitalWrite(D3, LOW);
-  digitalWrite(D4, LOW);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  delay(1000);
-  //1
-  digitalWrite(D1, HIGH);
-  delay(1000);
-  digitalWrite(D1, LOW);
-  //2
-  digitalWrite(D2, HIGH);
-  delay(1000);
-  digitalWrite(D2, LOW);
-  //3
-  digitalWrite(D1, HIGH);
-  digitalWrite(D2, HIGH);
-  delay(1000);
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-  //4
-  digitalWrite(D3, HIGH);
-  delay(1000);
-  digitalWrite(D3, LOW);
-  //5
-  digitalWrite(D3, HIGH);
-  digitalWrite(D1, HIGH);
-  delay(1000);
-  digitalWrite(D3, LOW);
-  digitalWrite(D1, LOW);
-  //6
-  digitalWrite(D3, HIGH);
-  digitalWrite(D2, HIGH);
-  delay(1000);
-  digitalWrite(D3, LOW);
-  digitalWrite(D2, LOW);
-  //7
-  digitalWrite(D3, HIGH);
-  digitalWrite(D2, HIGH);
-  digitalWrite(D1, HIGH);
-  delay(1000);
-  digitalWrite(D3, LOW);
-  digitalWrite(D2, LOW);
-  digitalWrite(D1, LOW);
-  //8
-  digitalWrite(D4, HIGH);
-  delay(1000);
-  digitalWrite(D4, LOW);
-  //9
-  digitalWrite(D4, HIGH);
-  digitalWrite(D1, HIGH);
-  delay(1000);
-  digitalWrite(D4, LOW);
-  digitalWrite(D1, LOW);
+  setValueToDigit(displayData[0], displayDigit[0]);
+  setValueToDigit(displayData[1], displayDigit[1]);
+  setValueToDigit(displayData[2], displayDigit[2]);
+  setValueToDigit(displayData[3], displayDigit[3]);
 }
